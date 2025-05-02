b6d7a53b800d4e6e2f23be253d4789d1a0d65774
Alfonso <alfonsorodriguez235@gmail.com>
2025-05-02 17:21:55 +0200

    First commit

---
+++ src/main/java/com/vs2dam/azarquiel/chocofonso_springboot/ChocofonsoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.vs2dam.azarquiel.chocofonso_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocofonsoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocofonsoSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vs2dam/azarquiel/chocofonso_springboot/ChocofonsoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vs2dam.azarquiel.chocofonso_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocofonsoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
