fef699c4c14c677453a61b8964cb9e9466153d88
Alfonso <alfonsorodriguez235@gmail.com>
2025-06-04 00:29:24 +0200

    Update Tablas BD

--- bd/categorias.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `categorias` (
-  `id_categoria` int NOT NULL AUTO_INCREMENT,
-  `nombre_categoria` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_categoria`),
-  UNIQUE KEY `nombre_categoria` (`nombre_categoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
